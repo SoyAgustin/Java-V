cd633baeb8effd8bf1b24e6a1fbc6149c41553c6
SoyAgustin <Agustinphysx@gmail.com>
2023-07-12 01:35:58 -0600

    Agrupando el proyecto en paquetes: modelo,test

---
+++ src/mx/com/bytebank/modelo/CuentaCorriente.java
@@ -0,0 +1,19 @@
+package mx.com.bytebank.modelo;
+
+public class CuentaCorriente extends Cuenta {
+	public CuentaCorriente(int agencia, int numero) {
+		//llamamos al constructor por defecto que recibe agencia y numero
+		super(agencia,numero);
+	}
+	
+	@Override
+	public boolean retirar(double valor) {
+		double comision = 0.2;
+		return super.retirar(valor+comision);//10 centavos de comisión 
+	}
+
+	@Override
+	public void depositar(double saldo) {
+		this.saldo += saldo;
+	}
+}
